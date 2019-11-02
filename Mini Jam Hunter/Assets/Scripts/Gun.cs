f14382b27900b640f58389cf25061f8c8de912e3
theInsomniacGameMaker <me@theinsomniacgamemaker.com>
2019-11-01 21:46:59 -0400

    Added Gun and Look At
    First versions of both the gun script and look at functionality.

---
+++ Mini Jam Hunter/Assets/Scripts/Gun.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Camera fpsCamera;
+
+    [SerializeField]
+    private float range = 100.0f;
+
+    [SerializeField]
+    private float damage;
+
+    [SerializeField]
+    private float fireRate = 15.0f;
+
+    private float nextTimeToFire = 0.0f;
+    private void Start()
+    {
+         
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time>nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1.0f / fireRate;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
---
+++ Mini Jam Hunter/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField]
+    private float mouseSensitivity = 100.0f;
+    [SerializeField]
+    private float clampAngle = 80.0f;
+
+    private float rottationY = 0.0f;
+    private float rotationX = 0.0f;
+
+    [SerializeField]
+    private bool allowMovement = false;
+
+    private void Start()
+    {
+        Vector3 startRotation = transform.localRotation.eulerAngles;
+        rottationY = startRotation.y;
+        rotationX = startRotation.x;
+    }
+
+    private void Update()
+    {
+        if (!allowMovement)
+        {
+            Rotate();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            allowMovement = !allowMovement;
+        }
+    }
+
+    public void Rotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rottationY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotationX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotationX = Mathf.Clamp(rotationX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotationX, rottationY, 0.0f);
+        transform.rotation = localRotation;
+
+    }
+}
