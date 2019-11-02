3da78856bdc3860a21b6425d5161453b466d7110
Dagger93 <jasondagger93@gmail.com>
2019-11-02 02:02:42 -0400

    Added pick up prefab, material, and script

---
+++ Mini Jam Hunter/Assets/Scripts/PickUp/PickUp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+
+    protected void OnCollisionEnter(Collision collision) {
+        
+    }
+}
