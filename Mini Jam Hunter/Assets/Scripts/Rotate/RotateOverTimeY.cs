7eb63e84bbbce8cd111bb70de45d858826674010
Dagger93 <jasondagger93@gmail.com>
2019-11-02 00:42:08 -0400

    Added Rotate Over Time on Y Axis

---
+++ Mini Jam Hunter/Assets/Scripts/Rotate/RotateOverTimeY.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTimeY : MonoBehaviour {
+
+    public float speedRotate;
+    public Space space;
+
+    public void Update() {
+        transform.Rotate(Vector3.up, speedRotate * Time.deltaTime, space);
+    }
+}
