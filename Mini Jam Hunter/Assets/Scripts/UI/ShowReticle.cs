96d65de311af7ec7957bf3c05f26e65b30977fe5
Dagger93 <jasondagger93@gmail.com>
2019-11-03 02:27:08 -0500

    Updated UI

---
+++ Mini Jam Hunter/Assets/Scripts/UI/ShowReticle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowReticle : MonoBehaviour
+{
+    Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    private void Update()
+    {
+        if (Controls.Instance.currentView == View.FirstPerson)
+        {
+            image.enabled = true;
+        }
+        else if (Controls.Instance.currentView == View.TopDown)
+        {
+            image.enabled = false;
+        }
+    }
+}
---
+++ Mini Jam Hunter/Assets/Scripts/UI/UIHealthFill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealthText : MonoBehaviour
+{
+    public Entity player;
+    private Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        text.text = player.GetHealthCurrent().ToString();
+    }
+}
