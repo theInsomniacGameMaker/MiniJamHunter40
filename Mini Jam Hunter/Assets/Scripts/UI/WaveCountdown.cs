53743535623da1365abb69f8c6ab80c4877c5118
Dagger93 <jasondagger93@gmail.com>
2019-11-03 19:21:44 -0500

    Updated UI

--- Mini Jam Hunter/Assets/Scripts/UI/WaveCountdown.cs
+++ Mini Jam Hunter/Assets/Scripts/UI/WaveCountdown.cs
@@ -6,17 +6,16 @@ using UnityEngine.UI;
 public class WaveCountdown : MonoBehaviour {
     [SerializeField] private float countdownDuration;
 
-    private Text text;
+    [SerializeField] private Text textCountdown;
     private float countdownDurationOriginal;
 
     void Start() {
-        text = GetComponent<Text>();
         countdownDurationOriginal = countdownDuration;
     }
 
     void Update() {
         countdownDuration -= Time.deltaTime;
-        text.text = ((int)countdownDuration).ToString();
+        textCountdown.text = countdownDuration.ToString();
         if (countdownDuration <= 0.0f) {
             gameObject.SetActive(false);
         }
