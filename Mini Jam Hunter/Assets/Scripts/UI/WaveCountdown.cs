4d80c71f85e0fa51daa01782e69ab8b9879d6110
Dagger93 <jasondagger93@gmail.com>
2019-11-03 19:09:54 -0500

    Added level start time countdown

---
+++ Mini Jam Hunter/Assets/Scripts/UI/WaveCountdown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveCountdown : MonoBehaviour {
+    [SerializeField] private float countdownDuration;
+
+    private Text text;
+    private float countdownDurationOriginal;
+
+    void Start() {
+        text = GetComponent<Text>();
+        countdownDurationOriginal = countdownDuration;
+    }
+
+    void Update() {
+        countdownDuration -= Time.deltaTime;
+        text.text = ((int)countdownDuration).ToString();
+        if (countdownDuration <= 0.0f) {
+            gameObject.SetActive(false);
+        }
+    }
+    void OnEnable() {
+        countdownDuration = countdownDurationOriginal;
+    }
+}
