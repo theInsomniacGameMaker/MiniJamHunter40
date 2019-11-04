adb839f37a68314110ebc9750060979cfc45acf7
Dagger93 <jasondagger93@gmail.com>
2019-11-03 21:36:29 -0500

    UI Score implemented (Death included as just for commit)

---
+++ Mini Jam Hunter/Assets/Scripts/UI/UIDeathText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDeathText : MonoBehaviour
+{
+    private Controls controls;
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+        controls = Controls.Instance;
+    }
+
+    private void Update()
+    {
+        text.text = "You died with a score of " + controls.score.ToString();
+    }
+}
